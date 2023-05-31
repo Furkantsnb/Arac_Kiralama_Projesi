88354c5c46557fdfc40779b0bb65213206df38c3
Furkan Taşan <95514574+Furkantsnb@users.noreply.github.com>
2023-05-31 13:12:18 +0300

    RENT A CAR

---
+++ RentACar/RentACar/Cars.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar
+{
+    abstract class Cars
+    {
+        public string Plate { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int YearOfProduction { get; set; }
+        public int km { get; set; }
+        public string Colour { get; set; }
+        public double RentMone { get; set; }
+        public bool Status { get; set; }
+
+        public abstract double CalculateRent(int day);
+        public abstract string Situation();
+        public bool VehicleİnspectionCheck()
+        {
+            if (Status)
+            {
+                Console.WriteLine("Araç muayenede olduğu için başka bir araç kiralayın.");
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+    // Araç sınıfı
+    class Car : Cars
+    {
+        public override double CalculateRent(int day)
+        {
+            double dailyRentalFee = 100;
+
+            return dailyRentalFee * day;
+        }
+
+        public override string Situation()
+        {
+            if (Status)
+                return " Hizmet dişi (Muayenede)";
+
+            else
+                return "Hizmette";
+        }
+    }
+
+    // Minübüs Sınıfı
+    class Minibus : Cars
+    {
+        public override Double CalculateRent(int day)
+        {
+            double dailyRentalFee = 200;
+
+            return dailyRentalFee * day;
+        }
+
+        public override string Situation()
+        {
+            if (Status)
+                return " Hizmet dişi (Muayenede)";
+
+            else
+                return "Hizmette";
+        }
+    }
+
+    // Kamyon sınıfı
+    class Truc : Cars
+    {
+        public override double CalculateRent(int day)
+        {
+            double dailRentalFree = 300;
+            return dailRentalFree * day;
+        }
+
+        public override string Situation()
+        {
+            if (Status)
+                return "Hizmet Dışı (Muayene)";
+            else
+                return "Hizmette";
+        }
+
+
+    }
+
+    // Araç kiralama servisi
+    class CarRentalService
+    {
+        private List<Cars> cars;
+        public CarRentalService()
+        {
+            cars = new List<Cars>();
+        }
+
+        public void AddCar(Cars car)
+        {
+            cars.Add(car);
+            Console.WriteLine("Araç başarıyla eklendi.");
+        }
+        public void Rent (string plate, int day)
+        {
+            Cars rentalCar = null;
+
+            //kiralanan aracı bulma
+            foreach (Cars car in cars)
+            {
+                if (car.Plate==plate)
+                {
+                    rentalCar = car;
+                    break;
+                }
+            }
+
+            if (rentalCar != null )
+            {
+                if (rentalCar.Status)
+                {
+
+                    double rentFree = rentalCar.CalculateRent(day);
+                    Console.WriteLine("Kiralanan Araç Bilgileri");
+                    Console.WriteLine("Plate : " + rentalCar.Plate);
+                    Console.WriteLine("Brand : " + rentalCar.Brand);
+                    Console.WriteLine("Model : " + rentalCar.Model);
+                    Console.WriteLine("Kiralama Gün Sayısı : " + day);
+                    Console.WriteLine("Toplam Kira Ücreti: " + rentFree);
+                }
+
+                else
+                {
+                    Console.WriteLine("Araç muayenede olduğu için başka bir araç kiralayın.");
+                }
+            }
+            else
+                Console.WriteLine("Belirtilen plakaya sahip bir araç bulunmamaktadır.");
+        }
+    }
+}
---
+++ RentACar/RentACar/Program.cs
@@ -0,0 +1,142 @@
+﻿namespace RentACar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarRentalService carRentalService = new CarRentalService();
+
+            // Örnek araçlar oluşturma ve sisteme ekleme
+            Car car = new Car();
+            car.Plate = "34 ABC 123";
+            car.Brand = "Renault";
+            car.Model = "Clio";
+            car.YearOfProduction = 2020;
+            car.km = 5000;
+            car.Colour = "Kırmızı";
+            car.Status = true;
+            carRentalService.AddCar(car);
+
+            Minibus minibüs = new Minibus();
+            minibüs.Plate = "34 DEF 456";
+            minibüs.Brand = "Mercedes";
+            minibüs.Model = "Sprinter";
+            minibüs.YearOfProduction = 2019;
+            minibüs.km = 10000;
+            minibüs.Colour = "Beyaz";
+            minibüs.Status = false;
+            carRentalService.AddCar(minibüs);
+
+            Truc truc = new Truc();
+            truc.Plate = "34 GHI 789";
+            truc.Brand = "Volvo";
+            truc.Model = "FH";
+            truc.YearOfProduction = 2021;
+            truc.km = 2000;
+            truc.Colour = "Mavi";
+            truc.Status = true;
+            carRentalService.AddCar(truc);
+
+            Console.WriteLine("Araç Kiralama Sistemi");
+            Console.WriteLine("----------------------");
+
+            while (true)
+            {
+                Console.WriteLine("1- Araç Ekle");
+                Console.WriteLine("2- Araç Kirala");
+                Console.WriteLine("3- Çıkış");
+                Console.Write("Seçiminizi yapın: ");
+                string option = Console.ReadLine();
+
+                if (option == "1")
+                {
+                    Console.WriteLine("Araç Bilgilerini Girin");
+
+                    Console.Write("Plaka: ");
+                    string plate = Console.ReadLine();
+
+                    Console.Write("Marka: ");
+                    string brand = Console.ReadLine();
+
+                    Console.Write("Model: ");
+                    string model = Console.ReadLine();
+
+                    Console.Write("Üretim Yılı: ");
+                    int yearOfProduction = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Kilometre: ");
+                    int km = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Renk: ");
+                    string colour = Console.ReadLine();
+
+                    Console.Write("Muayene Durumu (Evet/Hayır): ");
+                    bool status = Console.ReadLine().ToLower() == "evet";
+
+                    Console.Write("Araç Türü (Otomobil/Minibüs/Kamyon): ");
+                    string araçTürü = Console.ReadLine().ToLower();
+
+                    Cars arac = null;
+
+                    switch (araçTürü)
+                    {
+                        case "otomobil":
+                            arac = new Car();
+                            break;
+                        case "minibüs":
+                            arac = new Minibus();
+                            break;
+                        case "kamyon":
+                            arac = new Truc();
+                            break;
+                        default:
+                            Console.WriteLine("Geçersiz araç türü.");
+                            break;
+                    }
+
+                    if (car != null)
+                    {
+                        car.Plate = plate;
+                        car.Brand = brand;
+                        car.Model = model;
+                        car.YearOfProduction = yearOfProduction;
+                        car.km = km;
+                        car.Colour = colour;
+                        car.Status = status;
+
+                        carRentalService.AddCar(car);
+                    }
+                }
+
+
+                else if (option == "2")
+                {
+                    Console.Write("Kiralayacağınız aracın plakasını girin: ");
+                    string plate = Console.ReadLine();
+
+                    
+            
+                        Console.Write("Kiralama gün sayısını girin: ");
+                        int gün = Convert.ToInt32(Console.ReadLine());
+
+                        carRentalService.Rent(plate, gün);
+                    
+                   
+                }
+                else if (option == "3")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz seçim. Lütfen tekrar deneyin.");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Programdan çıkılıyor...");
+            Console.ReadLine();
+        }
+    }
+}
